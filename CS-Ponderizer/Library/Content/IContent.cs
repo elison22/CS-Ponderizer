ceaebb6cadbcce7c4fc6c7bd202220fe491166cb
Brandt Elison <behatdude@gmail.com>
2015-10-12 01:11:38 -0600

    Scaffolding for the classes that represent the actual things to ponderize

---
+++ CS-Ponderizer/Library/Content/IContent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Ponderizer.Library.Content
+{
+    interface IContent
+    {
+        string getText();
+        string getDisplayName();
+        string getFooter();
+    }
+}
---
+++ CS-Ponderizer/Library/Content/Reference/Reference.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Ponderizer.Library.Content.Reference
+{
+    [Serializable]
+    enum Source { SCRIPTURE, GENERAL_CONFERENCE, BOOK, MANUAL, MAGAZINE, OTHER };
+
+    [Serializable]
+    class Reference
+    {
+        public Source source { get; set; }
+        public string name { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string chapter { get; set; }
+        public string verse { get; set; }
+
+        public Reference() { }
+        public Reference(Source source, string name)
+        {
+            this.source = source;
+            this.name = name;
+        }
+        public Reference(Source source, string name, string title, string author) : this(source, name)
+        {
+            this.title = title;
+            this.author = author;
+        }
+        public Reference(Source source, string name, string title, string chapter, string verse) : this(source, name)
+        {
+            this.title = title;
+            this.chapter = chapter;
+            this.verse = verse;
+        }
+
+    }
+}
